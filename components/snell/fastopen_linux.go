821cf020e187d33760bb9007ae2c9d59dde91a40
noodlesandoa <noodlesandoa@gmail.com>
2025-12-17 23:13:49 -0500

    fix(tfo): close fd and log enable result
    - Close listener fd from TCPListener.File() to avoid leaks
    
    - Return setsockopt errors to caller and log success/failure

--- components/snell/fastopen_linux.go
+++ components/snell/fastopen_linux.go
@@ -1,4 +1,3 @@
-
 package snell
 
 import (
@@ -9,21 +8,33 @@ import (
 	log "github.com/golang/glog"
 )
 
+const tcpFastOpenOpt = 23 // TCP_FASTOPEN on Linux
+
 func setTcpFastOpen(lis net.Listener, enable int) error {
 	if tl, ok := lis.(*net.TCPListener); ok {
 		file, err := tl.File()
 		if err != nil {
 			return err
 		}
+		defer file.Close()
 		sysconn, err := file.SyscallConn()
 		if err != nil {
 			return err
 		}
-		return sysconn.Control(func(fd uintptr) {
-			if err := syscall.SetsockoptInt(int(fd), syscall.SOL_TCP, 23, enable); err != nil {
-				log.Warningf("failed to set TCP fastopen: %v\n", err)
-			}
+
+		var setErr error
+		ctrlErr := sysconn.Control(func(fd uintptr) {
+			setErr = syscall.SetsockoptInt(int(fd), syscall.SOL_TCP, tcpFastOpenOpt, enable)
 		})
+		if ctrlErr != nil {
+			return ctrlErr
+		}
+		if setErr != nil {
+			log.Warningf("failed to set TCP fastopen: %v\n", setErr)
+			return setErr
+		}
+		log.Infof("TCP fastopen enabled=%d on %s\n", enable, lis.Addr().String())
+		return nil
 	}
 	return errors.New("invalid listener")
 }
